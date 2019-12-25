1e26bba68bf2c4a70a648bc76b558f9cba593655
Jyotirmay <jyomishra99@gmail.com>
2019-12-26 00:40:08 +0530

    conneted the backend to the mongo database

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
+
 const app = express();
+
+mongoose.connect("mongodb+srv://Daxe:shinderu@cluster0-pjpjj.mongodb.net/node_angular?retryWrites=true&w=majority")
+.then(() => {
+  console.log('Connected to database');
+})
+.catch(() => {
+  console.log('Connection failed!');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,34 +27,40 @@ app.use((req, res, next) => {
     "GET, POST, PATCH, DELETE, OPTIONS"
   );
   next();
-})
+});
 
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
-    console.log(post);
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content
+    });
+    post.save();
     res.status(201).json({
       message: "Post added successfully",
     });
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: "unique1",
-      title: "First server-side code",
-      content: "This is coming from the server"
-    },
-    {
-      id: "unique2",
-      title: "Second server-side code",
-      content: "This is coming from the server"
-    }
-  ];
+app.get('/api/posts', (req, res, next) => {
+  Post.find().then(
+    documents => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: documents
 
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
+      });
+      //console.log(documents);
+    });
+
 });
+app.delete("/api/posts/:id", (req, res, next) => {
+  //console.log(req.params.id);
+  Post.deleteOne({_id: req.params.id}).then(
+    result => {
+      console.log(result);
+      res.status(200).json({ message: "Post deleted!" });
+      });
+
+});
+
 
 module.exports = app;
