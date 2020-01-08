0a1e55cca3624ed51732b4baf1d8ae9c7119537d
Jyotirmay <jyomishra99@gmail.com>
2020-01-09 00:05:25 +0530

    uploading and fetching image

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatToolbar,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostsService } from './posts/posts.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    HttpClientModule
+  ],
+  providers: [PostsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/backend/routes/posts.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const multer = require("multer");
+
+const Post = require('../models/post');
+
+const router = express.Router();
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(file)
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if(isValid){
+      error = null;
+    }
+
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    console.log(file)
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  }
+});
+
+const upload = multer({ storage: storage }).single("image");
+
+router.post("", upload , (req, res, next) => {
+
+  console.log(req.body)
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: createdPost._id
+    });
+  });
+
+});
+
+router.put("/:id", (req, res, next) => {
+
+  // console.log(req.body)
+
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Update successful!" });
+  });
+});
+
+router.get("", (req, res, next) => {
+  Post.find().then(
+    documents => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: documents
+
+      });
+      console.log(documents);
+    });
+
+});
+
+router.get("/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not found!' });
+    }
+  });
+});
+
+router.delete("/:id", (req, res, next) => {
+  //console.log(req.params.id);
+  Post.deleteOne({ _id: req.params.id }).then(
+    result => {
+      console.log(result);
+      res.status(200).json({ message: "Post deleted!" });
+    });
+
+});
+
+
+module.exports = router;
